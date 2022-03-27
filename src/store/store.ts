21d8867c6bcebe455970659a7e6e0dab2f0d5b2d
sarathdamaraju2007 <sarathdamaraju2007@gmail.com>
2022-03-27 15:41:50 +0530

    fix(builder): fix slider props

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { builderReducer } from "../slice";
+import { activeElementReducer } from "../slice/active-element";
 import { modalReducer } from "../slice/modals";
 
 export const store = configureStore({
   reducer: {
     builderConfig: builderReducer,
     modalReducer,
+    activeElement: activeElementReducer,
   },
 });
 
